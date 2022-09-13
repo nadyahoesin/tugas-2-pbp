31d46211757af0905e5b5aa9237d27272a14c780
Nadya Hoesin <nadyahoesin@icloud.com>
2022-09-13 19:54:08 +0700

    tinggal deploy

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
+from katalog.models import CatalogItem
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+        'catalog_items': data_katalog,
+        'nama': 'Nadya Hoesin',
+        'npm': '2106651673'
+    }
+
+    return render(request, "katalog.html", context)
