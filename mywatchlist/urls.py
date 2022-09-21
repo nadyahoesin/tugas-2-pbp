91fdde7823449f030bd29b84f51444460ff4e42a
Nadya Hoesin <nadyahoesin@icloud.com>
2022-09-21 18:57:56 +0700

    setelah loaddata

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -4,7 +4,7 @@ from mywatchlist.views import show_html, show_json, show_xml
 app_name = 'mywatchlist'
 
 urlpatterns = [
-    path('html', show_html, name='show_html'),
-    path('json', show_json, name='show_json'),
-    path('xml', show_xml, name='show_xml'),
+    path('html/', show_html, name='show_html'),
+    path('json/', show_json, name='show_json'),
+    path('xml/', show_xml, name='show_xml'),
 ]
